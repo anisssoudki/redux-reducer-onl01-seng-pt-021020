68348819f849a65b289f0f13e69ac4e02f8ba788
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-12-01 19:19:38 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,4 +1,8 @@
 export function manageFriends(state ={friends: []}, action){
    case: "ADD_FRIEND"
             return {...state, action.payload};
+            
+  case: "REMOVE_FRIEND
+            return {...state, action.payload"
+            
 }
